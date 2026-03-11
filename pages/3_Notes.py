bf231094bd02c123d1953c44a3fdcc10a0773019
Francisco Fernandez Amato <franciscofernandezz1999@gmail.com>
2026-03-10 23:11:54 -0300

    Update 3_Notes.py

--- pages/3_Notes.py
+++ pages/3_Notes.py
@@ -453,22 +453,22 @@ with right:
                 unsafe_allow_html=True,
             )
             st.markdown('<div class="note-image">', unsafe_allow_html=True)
-            st.image(n["imagen1"], width=600)
+            st.image(n["imagen1"], output_format="PNG")
             st.markdown('</div>', unsafe_allow_html=True)
             
             st.markdown(f'<div class="note-text">{n["texto_parte2"]}</div>', unsafe_allow_html=True)
             st.markdown('<div class="note-image">', unsafe_allow_html=True)
-            st.image(n["imagen2"], width=600)
+            st.image(n["imagen2"], output_format="PNG")
             st.markdown('</div>', unsafe_allow_html=True)
             
             st.markdown(f'<div class="note-text">{n["texto_parte3"]}</div>', unsafe_allow_html=True)
             st.markdown('<div class="note-image">', unsafe_allow_html=True)
-            st.image(n["imagen3"], width=600)
+            st.image(n["imagen1"], output_format="PNG")
             st.markdown('</div>', unsafe_allow_html=True)
             
             st.markdown(f'<div class="note-text">{n["texto_parte4"]}</div>', unsafe_allow_html=True)
             st.markdown('<div class="note-image">', unsafe_allow_html=True)
-            st.image(n["imagen4"], width=600)
+            st.image(n["imagen1"], output_format="PNG")
             st.markdown('</div>', unsafe_allow_html=True)
         else:
             # Notas normales sin imágenes
